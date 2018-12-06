d3d7ffe6b2f0a5d1390fb8408d4775d6eb22c341
Sue Kah Yong <skybach@gmail.com>
2018-12-06 09:44:21 +0800

    use openzeppelin-solidity npm package instead of old ethpm packages

--- contracts/PLCRFactory.sol
+++ contracts/PLCRFactory.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.4.20;
 
-import "tokens/eip20/EIP20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "./PLCRVoting.sol";
 import "./ProxyFactory.sol";
 
 contract PLCRFactory {
 
-  event newPLCR(address creator, EIP20 token, PLCRVoting plcr);
+  event newPLCR(address creator, ERC20 token, PLCRVoting plcr);
 
   ProxyFactory public proxyFactory;
   PLCRVoting public canonizedPLCR;
@@ -20,9 +20,9 @@ contract PLCRFactory {
   /*
   @dev deploys and initializes a new PLCRVoting contract that consumes a token at an address
   supplied by the user.
-  @param _token an EIP20 token to be consumed by the new PLCR contract
+  @param _token an ERC20 token to be consumed by the new PLCR contract
   */
-  function newPLCRBYOToken(EIP20 _token) public returns (PLCRVoting) {
+  function newPLCRBYOToken(ERC20 _token) public returns (PLCRVoting) {
     PLCRVoting plcr = PLCRVoting(proxyFactory.createProxy(canonizedPLCR, ""));
     plcr.init(_token);
 
@@ -32,30 +32,30 @@ contract PLCRFactory {
   }
   
   /*
-  @dev deploys and initializes a new PLCRVoting contract and an EIP20 to be consumed by the PLCR's
+  @dev deploys and initializes a new PLCRVoting contract and an ERC20 to be consumed by the PLCR's
   initializer.
-  @param _supply the total number of tokens to mint in the EIP20 contract
-  @param _name the name of the new EIP20 token
-  @param _decimals the decimal precision to be used in rendering balances in the EIP20 token
-  @param _symbol the symbol of the new EIP20 token
+  @param _supply the total number of tokens to mint in the ERC20 contract
+  @param _name the name of the new ERC20 token
+  @param _decimals the decimal precision to be used in rendering balances in the ERC20 token
+  @param _symbol the symbol of the new ERC20 token
   */
-  function newPLCRWithToken(
-    uint _supply,
-    string _name,
-    uint8 _decimals,
-    string _symbol
-  ) public returns (PLCRVoting) {
-    // Create a new token and give all the tokens to the PLCR creator
-    EIP20 token = new EIP20(_supply, _name, _decimals, _symbol);
-    token.transfer(msg.sender, _supply);
+  // function newPLCRWithToken(
+  //   uint _supply,
+  //   string _name,
+  //   uint8 _decimals,
+  //   string _symbol
+  // ) public returns (PLCRVoting) {
+  //   // Create a new token and give all the tokens to the PLCR creator
+  //   ERC20 token = new ERC20(_supply, _name, _decimals, _symbol);
+  //   token.transfer(msg.sender, _supply);
 
-    // Create and initialize a new PLCR contract
-    PLCRVoting plcr = PLCRVoting(proxyFactory.createProxy(canonizedPLCR, ""));
-    plcr.init(token);
+  //   // Create and initialize a new PLCR contract
+  //   PLCRVoting plcr = PLCRVoting(proxyFactory.createProxy(canonizedPLCR, ""));
+  //   plcr.init(token);
 
-    emit newPLCR(msg.sender, token, plcr);
+  //   emit newPLCR(msg.sender, token, plcr);
 
-    return plcr;
-  }
+  //   return plcr;
+  // }
 }
 
